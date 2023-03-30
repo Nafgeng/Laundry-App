c1916a0627ebd1ffbd47deef2d08d942654a0fcd
Dzikstarts <dzikryh071@gmail.com>
2023-03-30 08:00:56 +0700

    Create DataController.php

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    //
+}
+
+// private $taskList = [
+//     'first' => 'Eat',
+//     'second' => 'sleep',
+//     'third' => 'code'
+// ];
+
+// public function index()
+// {
+//     if (request()->search) {
+//         return $this->taskList[request()->search];
+//     }
+//     return $this->taskList;
+// }
+
+// public function store()
+// {
+//     $this->taskList[request()->label] = request()->tasks;
+
+//     return $this->taskList;
+// }
+
+// public function show($param)
+// {
+//     return $this->taskList[$param];
+// }
+
+// public function update($key)
+// {
+//     $this->taskList[$key] = request()->task;
+//     return $this->taskList;
+// }
+
+// public function destroy($key)
+// {
+//     unset($this->taskList[$key]);
+//     return $this->taskList;
+// }

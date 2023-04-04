47cb4576a7414768cef287fed9515f9a3666fc58
Dzikstarts <dzikryh071@gmail.com>
2023-04-04 13:54:13 +0700

    new

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,19 @@
+<div class="sidebar">
+    <div class="sidebar-logo">
+        <img src="img/logo.png" alt="">
+    </div>
+    <ul class="sidebar-list">
+        <li class="sidebar-list__item">
+            <div class="active-tabs-sidebar-list__item-button on">
+                <i class="fa-solid fa-file"></i>
+                <p>Data Laundry</p>
+            </div>
+        </li>
+        <li class="sidebar-list__item">
+            <div class="sidebar-list__item-button">
+                <i class="fa-solid fa-book-open"></i>
+                <p>Artikel Laundry</p>
+            </div>
+        </li>
+    </ul>
+</div>
--- resources/views/layouts/user_data-laundry.blade.php
+++ resources/views/layouts/user_data-laundry.blade.php
@@ -21,25 +21,7 @@
 <body>
     <div class="grid-container">
         <!-- SIDEBAR -->
-        <div class="sidebar">
-            <div class="sidebar-logo">
-                <img src="img/logo.png" alt="">
-            </div>
-            <ul class="sidebar-list">
-                <li class="sidebar-list__item">
-                    <div class="active-tabs-sidebar-list__item-button on">
-                        <i class="fa-solid fa-file"></i>
-                        <p>Data Laundry</p>
-                    </div>
-                </li>
-                <li class="sidebar-list__item">
-                    <div class="sidebar-list__item-button">
-                        <i class="fa-solid fa-book-open"></i>
-                        <p>Artikel Laundry</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
+        @include('components.sidebar')
 
         <div class="main">
                 <header>

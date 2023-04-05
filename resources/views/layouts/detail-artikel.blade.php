4313cbd61496c85e8e325b3aa794363bdd801f73
Nafgeng <95330932+Nafgeng@users.noreply.github.com>
2023-04-05 12:50:53 +0700

    detail artikel
    detail artikel + responsive

---
+++ resources/views/layouts/detail-artikel.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Artikel</title>
+
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/58b4b4ea39.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="navbar">
+        <a href=""><i class="fa-solid fa-chevron-left"></i></a>
+        <img src="logo.png" alt="logo">
+    </div>
+
+    <main>
+        <div class="header">
+            <div class="header_text">
+                <h4>Laundry</h4>
+            </div>
+            <div class="header_title">
+                <h1>5 Alasan Orang Pilih Jasa Laundry Ketimbang Nyuci Sendiri.</h1>
+            </div>
+            <div class="header_reason">
+                <p>Berikut 5 Alasan Mengapa Masyarakat Lebih Memilih Menggunakan Jasa Laundry Ketimbang Nyuci Sendiri.</p>
+            </div>
+        </div>
+
+        <img src="./img.jpg" alt="image content">
+
+        <div class="main_text">
+            <p>Dilansir dari <strong>PanturaPost.id</strong> - Meskipun mencuci pakaian merupakan aktivitas harian, namun beberapa ada yang menitipkan baju kotornya di tempat pencucian atau biasa kita sebut laundry. Kebanyakan dari pengguna jasa laundry menginginkan bajunya rapi dan wangi tanpa harus mencucinya di rumah.</p>
+
+            <p>Panturapost.id mendatangi salah satu penyedia jasa laundry di Brebes. Pemilik laundry Muhammad Sefiana dan istrinya Afi wahyuli menuturkan alasan orang menitipkan pakaian kotor. Berikut beberapa alasan orang menggunakan jasa laundry.</p>
+
+            <p><strong>1. Sibuk</strong><br>
+            Meskipun sudah mempunyai mesin cuci, sebagian orang merasa tidak sanggup mencuci sendiri. Ini kerap dilakukan bagi keluarga yang mempunyai banyak kesibukan.</p>
+
+            <p><strong>2. Cucian menumpuk</strong><br>
+            Pada momen tertentu, seperti akhir pekan saat berlibur ke luar kota, ketika pulang biasanya pakaian kotor tampak menumpuk. Ini menjadikan orang enggan untuk menyentuhnya. Alhasil mereka memilih untuk memanggil tukang cuci atau menitipkannya di tempat laundry.</p>
+
+            <p><strong>3. Pakaian tertentu saja yang di-laundry</strong><br>
+            Pemilik Laundry menuturkan, ada beberapa yang hanya menitipkan seragam sekolah dan pakaian kerja untuk mendapatkan hasil yang maksimal, sehingga rapi dan wangi. "Ada beberapa yang cuma nitip seragam anaknya, kalau warna putih kita pisahkan," tuturnya.</p>
+
+            <p><strong>4. Mencuci karpet</strong><br>
+            Selain pakaian, tempat laundry juga menerima jasa cuci karpet. Kebanyakan orang tidak mau bersusah payah mencuci karpet yang tergolong berat. "Kalau karpet tarifnya per meter," ujar Sefiana.</p>
+
+            <p><strong>5. Orang yang ngekost</strong><br>
+            Bagi orang yang bekerja jauh dari rumahnya, persoalan mencuci mungkin menjadi kendala tersendiri. Untuk itu mereka sangat memanfaatkan jasa laundry ini. "Banyak juga anak kost yang nyuci di sini, katanya sibuk kerja dan tidak sempat nyuci," jelas pemilik loundry.</p>
+        </div>
+    </main>
+</body>
+</html>

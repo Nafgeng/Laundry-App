537eec1cdcc8fbdd2b388226e03a33960b3b2aa0
Nafgeng <95330932+Nafgeng@users.noreply.github.com>
2023-04-05 12:48:55 +0700

    artikel
    artikel + responsive

---
+++ resources/views/layouts/artikel.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href=css/artikel.css >
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/c6a35e66f0.js" crossorigin="anonymous"></script>
+
+    <title>Laundry - User Data</title>
+
+</head>
+
+<body>
+    <div class="grid-container">
+        <!-- SIDEBAR -->
+        <div class="sidebar">
+            <div class="sidebar-logo">
+                <img src="image/logo.png" alt="">
+            </div>
+            <ul class="sidebar-list">
+                <li class="sidebar-list__item">
+                    <div class="active-sidebar-list__item-button">
+                        <i class="fa-solid fa-file"></i>
+                        <p>Data Laundry</p>
+                    </div>
+                </li>
+                <li class="sidebar-list__item">
+                    <div class="sidebar-list__item-button">
+                        <i class="fa-solid fa-book-open"></i>
+                        <p>Artikel</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="main">
+            <header>
+                <div class="menu-icon">
+                    <i class="fa-solid fa-bars"></i>
+                </div>
+
+                <div class="header-right">
+                    <div class="header-right__text">
+                        <p>User Account</p>
+                        <p>Log Out</p>
+                    </div>
+                        <i class="fa-solid fa-circle-user"></i>
+                    </div>
+
+            </header>
+
+            <div class="main-title">
+                <h1>Baca Artikel</h1>
+                <h3>Kumpulan Artikel - Artikel Tentang Laundry</h3>
+            </div>
+
+            <div class="artikel">
+                <img src="img1.jpg" alt="artikel image">
+                <div class="artikel-item">
+                    <div class="artikel-title">
+                        <h1>5 Alasan Orang Pilih Jasa Laundry Ketimbang Nyuci Sendiri.</h1>
+                    </div>
+                    <div class="artikel-desc">
+                        <p>Meskipun mencuci pakaian merupakan aktivitas harian, namun beberapa ada </p>
+                    </div>
+                    <div class="btn"><i class="fa-solid fa-book-open"></i>
+                    <a href="">Baca Artikel</a></div>
+                </div>
+            </div>
+
+            <div class="grid-list">
+                <div class="grid-item">
+                    <img src="list1.jpg" alt="artikel image">
+                    <div class="grid-text">
+                        <div class="grid-title">
+                            <h2>Pengertian Laundry : Tugas,
+                                Struktur Organisasi dan SOP</h2>
+                        </div>
+                        <div class="grid-value">
+                            <p>Laundry (binatu) merupakan salah satu bagian dari department</p>
+                        </div>
+                    </div>
+                    <div class="btn-grid"><i class="fa-solid fa-book-open"></i>
+                        <a href="">Baca Artikel</a></div>
+                </div>
+                <div class="grid-item">
+                    <img src="list2.jpg" alt="artikel image">
+                    <div class="grid-text">
+                        <div class="grid-title">
+                            <h2>Pengertian Laundry : Tugas,
+                                Struktur Organisasi dan SOP</h2>
+                        </div>
+                        <div class="grid-value">
+                            <p>Laundry (binatu) merupakan salah satu bagian dari department</p>
+                        </div>
+                    </div>
+                    <div class="btn-grid"><i class="fa-solid fa-book-open"></i>
+                        <a href="">Baca Artikel</a></div>
+                </div>
+                <div class="grid-item">
+                    <img src="list3.jpg" alt="artikel image">
+                    <div class="grid-text">
+                        <div class="grid-title">
+                            <h2>Pengertian Laundry : Tugas,
+                                Struktur Organisasi dan SOP</h2>
+                        </div>
+                        <div class="grid-value">
+                            <p>Laundry (binatu) merupakan salah satu bagian dari department</p>
+                        </div>
+                    </div>
+                    <div class="btn-grid"><i class="fa-solid fa-book-open"></i>
+                        <a href="">Baca Artikel</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- custom js     -->
+    <script src="js/script.js"></script>
+
+</body>
+</html>

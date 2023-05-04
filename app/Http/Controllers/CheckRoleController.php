cc835f84e644591744ebb9b5f3355b350d2bfe31
Alifmslm <alifmslm01@gmail.com>
2023-05-04 21:11:23 +0700

    Make Middleware isadmin and isuser

---
+++ app/Http/Controllers/CheckRoleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckRoleController extends Controller
+{
+
+    public function check_role()
+    {
+        $role = Auth::user()->role;
+
+        if ($role == 'admin') {
+            return redirect()->action([AdminController::class, 'admin_data_laundry']);
+        } else {
+            return redirect()->action([HomeController::class, 'user_data_laundry']);
+        }
+    }
+}
